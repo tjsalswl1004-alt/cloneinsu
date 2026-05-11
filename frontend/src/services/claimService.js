2b0c7a434175aa793551cd52c9e81f268d2e58be
sunminji <tjsalswl1004@naver.com>
2026-05-11 10:27:37 +0900

    feat: add claimService with axios

---
+++ frontend/src/services/claimService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+
+export const claimService = {
+  getAll: (status) =>
+    api.get('/claims', { params: status ? { status } : {} }).then((r) => r.data),
+
+  getById: (id) =>
+    api.get(`/claims/${id}`).then((r) => r.data),
+
+  create: (data) =>
+    api.post('/claims', data).then((r) => r.data),
+
+  update: (id, data) =>
+    api.put(`/claims/${id}`, data).then((r) => r.data),
+
+  remove: (id) =>
+    api.delete(`/claims/${id}`),
+
+  getStats: () =>
+    api.get('/claims/stats').then((r) => r.data),
+};
